20cf118c189b9a589fb929553ab59d002a2e740b
Chikaodili Deng <mchikky@yahoo.com>
2023-03-14 16:22:59 +0100

    Initial Commit

---
+++ Design5/Design5App.swift
@@ -0,0 +1,17 @@
+//
+//  Design5App.swift
+//  Design5
+//
+//  Created by Chikaodili Deng on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Design5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

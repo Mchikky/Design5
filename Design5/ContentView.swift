c9152c97af4629c878dc85e2a80ca78a1ff19118
Chikaodili Deng <mchikky@yahoo.com>
2023-03-17 14:57:56 +0100

    Bouton Binding

--- Design5/ContentView.swift
+++ Design5/ContentView.swift
@@ -8,19 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text = "Select a suit"
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            HStack(spacing: 30) {
+                Button {
+                    text = "♣️"
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius:20)
+                            .fill(.black)
+                            .frame(width:120, height: 30)
+                        HStack {
+                            Image(systemName: "suit.club.fill")
+                                .imageScale(.large)
+                                .foregroundColor(.white)
+                            Text("club")
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                Button {
+                    text = "♠️"
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius:20)
+                            .fill(.black)
+                            .frame(width:120, height: 30)
+                        HStack {
+                            Image(systemName: "suit.spade.fill")
+                                .imageScale(.large)
+                                .foregroundColor(.white)
+                            Text("spade")
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                
+            }
+            Spacer()
+            Text(text)
+                .foregroundColor(.gray)
+                .font(.title2)
+            Spacer()
+            HStack(spacing: 30){
+                Button {
+                    text = "♦️"
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius:20)
+                            .fill(.red)
+                            .frame(width:120, height: 30)
+                        HStack {
+                            Image(systemName: "suit.diamond.fill")
+                                .imageScale(.large)
+                                .foregroundColor(.white)
+                            Text("diamond")
+                                .foregroundColor(.white)
+                        }
+                        
+                    }
+                }
+                Button {
+                    text = "❤️"
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius:20)
+                            .fill(.red)
+                            .frame(width:120, height: 30)
+                        HStack {
+                            Image(systemName: "heart.fill")
+                                .imageScale(.large)
+                                .foregroundColor(.white)
+                            Text("heart")
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+            }
+            Spacer()
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
